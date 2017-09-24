1ef5931894a1640354247713c09b14316bab2315
katherine0504 <F74036069@ncku.edu.tw>
2017-09-24 11:44:01 +0800

    Mpdify layout

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-(function($) {
-    "use strict";
-
-    // manual carousel controls
-    $('.north-next').click(function(){ $('.carousel-north').carousel('next');return false; });
-    $('.north-prev').click(function(){ $('.carousel-north').carousel('prev');return false; });
-    $('.carousel-north').carousel({interval: false});
-    $('.mid-next').click(function(){ $('.carousel-mid').carousel('next');return false; });
-    $('.mid-prev').click(function(){ $('.carousel-mid').carousel('prev');return false; });
-    $('.carousel-mid').carousel({interval: false});
-    $('.south-next').click(function(){ $('.carousel-south').carousel('next');return false; });
-    $('.south-prev').click(function(){ $('.carousel-south').carousel('prev');return false; });
-    $('.carousel-south').carousel({interval: false});
-})(jQuery);
+$(document).ready(function() {
+    $('.crop-img').each(function() {
+    var maxWidth = $('.card').width(); // 圖片最大寬度
+    var maxHeight = 300;    // 圖片最大高度
+    var ratio = 0;  // 縮放比例
+    var width = $(this).width();    // 圖片實際寬度
+    var height = $(this).height();  // 圖片實際高度
+ 
+    // 檢查圖片是否超寬
+    if(width > maxWidth){
+        ratio = maxWidth / width;   // 計算縮放比例
+        $(this).css("width", maxWidth); // 設定實際顯示寬度
+        height = height * ratio;    // 計算等比例縮放後的高度 
+        $(this).css("height", height * ratio);  // 設定等比例縮放後的高度
+    }
+ 
+    // 檢查圖片是否超高
+    if(height > maxHeight){
+        ratio = maxHeight / height; // 計算縮放比例
+        $(this).css("height", maxHeight);   // 設定實際顯示高度
+        width = width * ratio;    // 計算等比例縮放後的高度
+        $(this).css("width", width * ratio);    // 設定等比例縮放後的高度
+    }
+});
+});
